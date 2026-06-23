d96207eeb2b205b970286c90c99f7cb3d193dff5
Rafael Sant'Ana <rafael.santana@dcc.ufmg.br>
2026-06-23 18:31:33 -0300

    Create FCFS handler and create requests channel

--- main.go
+++ main.go
@@ -1,25 +1,22 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	initialPosition := 256
 	maxDiskBytes := 512
-	requests := []int{315, 100, 15, 480, 0, 500, 15}
 
-	processRequests := ProcessRequestsDTO{
-		requests:        requests,
+	requests := make(chan int)
+
+	diskInfo := DiskInfo{
 		initialPosition: initialPosition,
 		maxDiskBytes:    maxDiskBytes,
 	}
 
-	totalMovementSCAN := scan(processRequests)
-	totalMovementFCFS := fcfs(processRequests)
-	totalMovementSSTF := sstf(processRequests)
+	go func() {
+		requests <- 100
+	}()
+	FCFSHandler := NewFCFSHandler(diskInfo)
 
-	fmt.Println("Total movement for SCAN:", totalMovementSCAN)
-	fmt.Println("Total movement for FCFS:", totalMovementFCFS)
-	fmt.Println("Total movement for SSTF:", totalMovementSSTF)
+	FCFSHandler.listenForMessages(requests)
+
+	//FCFSHandler.handle()
 }
---
+++ random-generator.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math/rand/v2"
+)
+
+func InsertRandomNumberInList() {
+	num := rand.IntN(100)
+
+	fmt.Println(num)
+}
